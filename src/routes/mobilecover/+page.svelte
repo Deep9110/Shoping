102a125ba1d841246596093918f7a2e27b539b16
Deep <99deepsundar@gmail.com>
2022-10-24 10:40:36 +0530

    data for mobilecover through api

--- src/routes/mobilecover/+page.svelte
+++ src/routes/mobilecover/+page.svelte
@@ -1,11 +1,30 @@
 <script>
+  import EachItems from "$lib/components/EachItems.svelte";
+import { stringify } from "postcss";
+  import { children } from "svelte/internal";
+
   export let data = { megamenu: [] }
 </script>
 
 <div class="menu">
   {#if data?.megamenu}
+  
+
     {#each data.megamenu as menuitem}
-      {JSON.stringify(menuitem)}
+      <!-- {JSON.stringify(menuitem)} -->
+      {menuitem.name}
+        {#each menuitem.children as childelement}
+          <div>
+            {childelement.name}
+            <div>
+              {#each childelement.children as subchild}
+                {subchild.name}
+              {/each}
+            </div>
+          </div>
+         
+        {/each}
     {/each}
+ 
   {/if}
 </div>
